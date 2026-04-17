de60f648f5f4eb9fc8f3c985058c790c94e2999d
SriramSRB <ssrirambabu27@gmail.com>
2026-04-17 17:24:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'library.html')));
 
-// Start the server
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'library.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Library Website Server is running!`);
+    console.log(`Access it locally at: http://localhost:${PORT}`);
 });
